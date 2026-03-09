8a73b8d1d45809822c95dd966af1439aa2b33a03
David Martini <dmartini@redhat.com>
2026-03-09 11:21:38 +0000

    add hta

---
+++ content/script-custom.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Custom script for OpenShift Virtualization Lab"
+
+oc whoami --show-server=true
+
+# Add IDP
+echo "Add IDP"
+
+oc patch oauth cluster --type=merge -p '{"spec":{"identityProviders":[{"name":"lab-beginner","mappingMethod":"claim","type":"HTPasswd","htpasswd":{"fileData":{"name":"htpass-beginner"}}}]}}'
