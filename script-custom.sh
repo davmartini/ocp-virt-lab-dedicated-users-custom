189bd0d25a0cd37b1e8eb21c3026ea8c80b175e3
David Martini <dmartini@redhat.com>
2026-03-09 11:27:49 +0000

    modify idp

--- script-custom.sh
+++ script-custom.sh
@@ -6,5 +6,5 @@ oc whoami --show-server=true
 
 # Add IDP
 echo "Add IDP"
-
-oc patch oauth cluster --type=merge -p '{"spec":{"identityProviders":[{"name":"lab-beginner","mappingMethod":"claim","type":"HTPasswd","htpasswd":{"fileData":{"name":"htpass-beginner"}}}]}}'
+oc apply -f gitops/htpasswd.yaml
+oc patch oauth cluster --type=merge -p '{"spec":{"identityProviders":[{"name":"lab-beginner","mappingMethod":"claim","type":"HTPasswd","htpasswd":{"fileData":{"name":"htpass-beginner"}}},{"name":"lab-advanced","mappingMethod":"claim","type":"HTPasswd","htpasswd":{"fileData":{"name":"htpasswd"}}}]}}'
